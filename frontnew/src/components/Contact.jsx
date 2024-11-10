2579e9b0d7beb0614d2399783b58b01707573299
NagaSowmyaSri <chnsowmyasri@gmail.com>
2024-11-10 23:47:41 +0530

    Update Contact.jsx

--- frontnew/src/components/Contact.jsx
+++ frontnew/src/components/Contact.jsx
@@ -56,8 +56,7 @@ const Contact = () => {
         <div className="banner">
           <div className="item">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57929.23355645912!2d67.01519255!3d24.8441321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e70a31f45a9%3A0x25e252450977ec12!2sSaddar%20Town%2C%20Karachi%2C%20Sindh!5e0!3m2!1sen!2s!4v1709099958323!5m2!1sen!2s"
-              style={{ border: 0, width: "100%", height: "450px" }}
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.8393498565!2d77.06889955!3d28.5275198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1db255555555%3A0x123456789abcdef!2sDelhi%2C%20India!5e0!3m2!1sen!2s!4v1709099958323!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"              style={{ border: 0, width: "100%", height: "450px" }}
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
